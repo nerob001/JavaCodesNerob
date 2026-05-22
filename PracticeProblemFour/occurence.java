b8ca69f8507dd12eff6df2ec04163f58712f1aff
nerob001 <nirabnondi67@gmail.com>
2026-05-22 13:02:28 +0600

    Create occurence.java to count character occurrences
    Add a Java program to count occurrences of a character in a string.

---
+++ PracticeProblemFour/occurence.java
@@ -0,0 +1,22 @@
+package FInalPracticeForLab;
+
+public class occurence {
+    public static void main(String[] args) {
+
+        String s = "Programming";
+        char ch = 'g';
+
+        int count = 0;
+
+        for(int i = 0;i<s.length();i++){
+
+            if(s.charAt(i) == ch){
+
+                count++;
+
+            }
+        }
+
+        System.out.println(count);
+    }
+}
