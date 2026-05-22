33d0dc4addc1563d3889a44fdddd69cf45c7b978
nerob001 <nirabnondi67@gmail.com>
2026-05-22 10:50:27 +0600

    Add Triangle class to calculate area

--- PracticeproblemOne/Triangle.java
+++ PracticeproblemOne/Triangle.java
@@ -1,1 +1,19 @@
 
+package PracticeOne;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter : ");
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        double area = 0.5 * base * height;
+
+        System.out.println("Area is : " + area);
+
+    }
+}
