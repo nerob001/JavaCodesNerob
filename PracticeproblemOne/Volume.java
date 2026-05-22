ae223815058aeeab2cf151f5be5afad2635dab49
nerob001 <nirabnondi67@gmail.com>
2026-05-22 10:53:08 +0600

    Add Volume calculation for a sphere

--- PracticeproblemOne/Volume.java
+++ PracticeproblemOne/Volume.java
@@ -1,1 +1,17 @@
 
+package PracticeOne;
+
+import java.util.Scanner;
+
+public class volume {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        final float PI = 3.1416F;
+
+        double volume = 4/3.0F * PI * radius  * radius * radius;
+
+        System.out.println("Volume is : "  + volume);
+    }
+}
